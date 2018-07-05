2d73f61af3ebd625fcc5e7a79c25450fd9028099
phoenixjessiestryker <phoenixjstryker@gmail.com>
2018-07-06 11:40:09 +1200

    getting api request player data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 
-import { getFruits } from '../apiClient'
+// importing superagent
+// import request from 'superagent'
+
+import { getPlayersData } from '../apiClient'
+
+// Components
+import Stats from './Stats'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      playerOne: {},
+      playerTwo: {}
+    }
   }
+
+  handleClick(e) {
+    e.preventDefault()
+
+    const input = document.getElementById('').value
+
+    const platform = e.target.elements.platform.value
+    const epicName = e.target.elements.epicName.value
+    // Full URL https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}
+    // String literal conversion *dabs* `{url/${platform}/${epicName}}`
+  
+    getPlayersData(playerOneName, playerOnePlatform)
+      .then(playerOneData => {
+        this.setState({
+          playerOne: playerOneData,
+          playerTwo: {}
+        })
+      })
+}
 
   // componentDidMount () {
 
@@ -15,6 +44,7 @@ class App extends React.Component {
     return (
 
       <React.Fragment>
+        <Stats getPlayerData={this.GetPlayerData}/>
         <h1>Hello people</h1>
       </React.Fragment>
 
